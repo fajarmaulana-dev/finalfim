aaeeb22438b99f5dcac7bc1df798c80c0e2ad821
fajarmaulana-dev <fajarmaulana.dev@gmail.com>
2022-11-02 01:18:45 +0700

    add loading lazy on notfound page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -4,7 +4,8 @@ import router from '../router';
 </script>
 <template>
     <div class="main">
-        <img src="@/assets/404.png" alt="404 image" style="min-width: 10rem; width: 60vw; max-width: 50vh;" />
+        <img loading="lazy" src="@/assets/404.png" alt="404 image"
+            style="min-width: 10rem; width: 60vw; max-width: 50vh;" />
         <p style="margin: 3rem 0 1rem 0; font-size: var(--card-title); font-weight: 700;">Kembali ke beranda ?</p>
         <Button :round="true" style="min-width: 10rem; width: 60vw; max-width: 50vh;" @click="router.push('/')">
             <i class="fa-solid fa-house-chimney" style="margin-right: .5rem;"></i> Home
