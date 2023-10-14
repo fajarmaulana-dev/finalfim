b72d3309e208d3d6d89b7efb51bb26fa9508781f
felkira <fajarmaulana_math@students.unnes.ac.id>
2023-10-14 13:53:03 +0700

    reduce the number of participants

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -15,7 +15,7 @@ const refreshCookie = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   maxAge: 30 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   secure: process.env.NODE_ENV === "production",
 };
 
@@ -23,7 +23,7 @@ const accessCookie = {
   expires: new Date(Date.now() + 10 * 60 * 1000),
   maxAge: 10 * 60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   secure: process.env.NODE_ENV === "production",
 };
 
